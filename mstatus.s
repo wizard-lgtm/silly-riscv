a170719a874802ba6930f9c11e4a3f97317f7660
ziggywizard <rippiten23@gmail.com>
2024-09-21 03:19:39 +0300

    mstatus changed to sstatus for idk

--- mstatus.s
+++ mstatus.s
@@ -2,7 +2,7 @@
     .globl _start
 
 _start:
-    csrr a0, mstatus
+    csrr a0, sstatus
     # Exit the program (use sys_exit syscall)
     li a7, 93         # sys_exit syscall number
     ecall             # Exit
