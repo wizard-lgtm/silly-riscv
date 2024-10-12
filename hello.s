6eb99288c5a426886e384042a861ef49cf715039
J <rippiten23@gmail.com>
2024-10-12 03:37:41 +0300

    Update hello.s

--- hello.s
+++ hello.s
@@ -1,24 +1,21 @@
-# this would be the most basic hello world app for every programming tutorial
-# but you know, we have to do it
+# classic hello world program in riscv64 linux
+# check the syscall table down description below
+.section .data
+message: .ascii "Hello world\n"
 
-.section .data # Constant data section
-    message: .ascii "Hello world!\n" # our message
-
-.section .text # Code section
-.globl _start # code start section
-
-# Argument registers are going through a0 to a7 
-# a0..a6 -> arguments
-# a7 -> syscall number
+.section .text
+.globl _start
 _start:
-    # Make Write syscall 
-    li a0, 1 # stdout file desctiptor 
-    la a1, message # load pointer of message
-    li a2, 13 # load length of the message
-    li a7, 64 # riscv linux write syscall 
-    ecall # make the syscall 
+	# Write syscall
+	li a7, 64 # write syscall number
+	la a0, message # our messages address
+	li a1, 1 # stdout number for linux
+	li a2, 12 # length of our message
 
-    # Make Exit syscall 
-    li a0, 0 # 0 code for exiting as successfully (no error)
-    li a7, 93 # riscv linux exit syscall
-    ecall # make the syscall
+	ecall # make system call
+
+	# Exit syscall
+	li a7, 93 # exit syscall number
+	li a0, 0 # 0 for successfull exit code
+
+	ecall # make system call
